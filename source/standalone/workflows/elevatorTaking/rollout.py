cea58bbb8c337c64832e02d32f7e7d2d7817e72c
Chenyu Yang <chenyang@student.ethz.ch>
2023-06-14 23:22:30 +0200

    refine the resume function in rollout

--- source/standalone/workflows/elevatorTaking/rollout.py
+++ source/standalone/workflows/elevatorTaking/rollout.py
@@ -45,6 +45,7 @@ import omni.isaac.contrib_envs  # noqa: F401
 import omni.isaac.orbit_envs  # noqa: F401
 from omni.isaac.orbit_envs.utils import parse_env_cfg
 from omni.isaac.orbit.utils.io import dump_pickle, dump_yaml, load_pickle, load_yaml
+from omni.isaac.orbit.utils import update_class_from_dict
 from glob import glob
 import re
 import os
@@ -60,6 +61,39 @@ def parse_log_line(line):
     # Remove the newline character from the timestamp
     timestamp = timestamp.strip()
     return int(epoch), timestamp
+
+
+def compare_dicts(dict1, dict2, path=""):
+    # Check if both are dictionaries
+    if not (isinstance(dict1, dict) and isinstance(dict2, dict)):
+        if dict1 != dict2:
+            print(f"Different values at path '{path}': {dict1} != {dict2}")
+        return
+    
+    # Check if both dictionaries have the same keys
+    keys1 = set(dict1.keys())
+    keys2 = set(dict2.keys())
+    
+    # Check for keys in dict1 but not in dict2
+    for key in keys1 - keys2:
+        print(f"Key '{key}' found in first dict but not in second at path '{path}'")
+    
+    # Check for keys in dict2 but not in dict1
+    for key in keys2 - keys1:
+        print(f"Key '{key}' found in second dict but not in first at path '{path}'")
+    
+    # Recursively compare values
+    for key in keys1 & keys2:
+        new_path = f"{path}.{key}" if path else key
+        value1 = dict1[key]
+        value2 = dict2[key]
+        
+        # If values are both dictionaries, recursively compare
+        if isinstance(value1, dict) and isinstance(value2, dict):
+            compare_dicts(value1, value2, new_path)
+        # Otherwise, check for equality
+        elif value1 != value2:
+            print(f"Different values at path '{new_path}': {value1} != {value2}")
 
 def main():
     """Run a trained policy from robomimic with Isaac Orbit environment."""
@@ -78,8 +112,6 @@ def main():
     env_cfg.initialization.robot.position_cat = "uniform"
     env_cfg.initialization.elevator.moving_elevator_prob = 0
     env_cfg.initialization.elevator.nonzero_floor_prob = 1
-    env_cfg.initialization.robot.position_uniform_min = [1.4, 0.9, -1.6]  # position (x,y,z)
-    env_cfg.initialization.robot.position_uniform_max = [1.6, 1.1, -1.4]  # position (x,y,z)
     env_cfg.terminations.is_success = "pushed_btn"
 
 
@@ -88,10 +120,15 @@ def main():
         print("Resuming from a previous rollout.", args_cli.resume)
         rollout_log = args_cli.resume
         log_dir = os.path.dirname(rollout_log)
-        env_cfg = load_pickle(os.path.join(log_dir, "env.pkl"))
+        _env_cfg = load_pickle(os.path.join(log_dir, "env.pkl"))
+        # These two paths sometimes changes between http://omniverse-content-productionxxxxx and omniverse://localhost/N
+        _env_cfg.robot.meta_info.usd_path = env_cfg.robot.meta_info.usd_path
+        _env_cfg.marker.usd_path = env_cfg.marker.usd_path
+        env_cfg = _env_cfg
         with open(os.path.join(log_dir, 'rollout_log.txt'), 'r') as logf:
+            args_cli.checkpoints = logf.readline().strip()
             done_files = [
-                parse_log_line(line)[0] for line in logf
+                parse_log_line(line)[0] for line in logf.readlines()
             ]
     else:
         log_dir = datetime.now().strftime("%b%d_%H-%M-%S")
@@ -100,10 +137,12 @@ def main():
         dump_yaml(os.path.join(log_dir, "env.yaml"), env_cfg)
         dump_pickle(os.path.join(log_dir, "env.pkl"), env_cfg)
         done_files = []
+        with open(os.path.join(log_dir, 'rollout_log.txt'), 'a') as logf:
+            logf.write(f"{args_cli.checkpoints}\n")
 
     # create environment
     env = myEnvGym(args_cli.task, cfg= env_cfg, headless= False)
-    
+
     # acquire device
     device = TorchUtils.get_torch_device(try_to_use_cuda=True)
 
